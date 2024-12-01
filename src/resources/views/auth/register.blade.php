3997b1df4c18e080944da09f696e5f8926df3558
seiko <seiko.iwao.0715@gmail.com>
2024-12-01 09:15:41 +0900

    ヘッダーの表示きりかえ

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -3,11 +3,13 @@
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/register.css') }}">
 @endsection
-
+@section('iwao')
+<a>login</a>
+@endsection
 @section('content')
 <div class="register__content">
   <div class="register-form__heading">
-    <h2>会員登録</h2>
+    <h2>Register</h2>
   </div>
  <form class="form" action="/register" method="post">
     @csrf
@@ -56,22 +58,9 @@
         </div>
       </div>
     </div>
-    <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">確認用パスワード</span>
-      </div>
-      <div class="form__group-content">
-        <div class="form__input--text">
-          <input type="password" name="password_confirmation" />
-        </div>
-      </div>
-    </div>
     <div class="form__button">
       <button class="form__button-submit" type="submit">登録</button>
     </div>
   </form>
-  <div class="login__link">
-    <a class="login__button-submit" href="/login">ログインの方はこちら</a>
-  </div>
 </div>
 @endsection
