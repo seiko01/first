d37f8327a7dcbaf33f735d50f6272773fb0c0b9e
seiko <seiko.iwao.0715@gmail.com>
2024-12-15 07:59:39 +0900

    thanksページ

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,5 +5,7 @@ use App\Http\Controllers\ContactController;
 
 Route::get('/', [ContactController::class, 'index']);
 Route::post('/contacts/confirm', [ContactController::class, 'confirm'])->name('contacts.confirm');
-Route::post('/contacts/store', [ContactController::class, 'store'])->name('contacts.store');
 Route::post('/contacts', [ContactController::class, 'store']);
+Route::get('/thanks', function () {
+    return view('thanks');
+})->name('thanks');
