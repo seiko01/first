5490767d5454beb097ea84d461cebd36dbe8cc0a
seiko <seiko.iwao.0715@gmail.com>
2024-12-14 21:44:23 +0900

    thanks.css変更中

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -5,27 +5,20 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Contact Form</title>
-  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
-  <link rel="stylesheet" href="{{ asset('css/thanks.css') }}" />
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>thank you</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
 </head>
 
 <body>
-  <header class="header">
-    <div class="header__inner">
-      <a class="header__logo" href="/">
-        Contact Form
-      </a>
+  <header class="thanks-page">
+    <div class="thanks-background">Thank you</div>
+    <div class="thanks-content">
+      <h1>お問い合わせありがとうございました</h1>
+      <a href="/" class="home-button">HOME</a>
     </div>
   </header>
-
-  <main>
-    <div class="thanks__content">
-      <div class="thanks__heading">
-        <h2>お問い合わせありがとうございます</h2>
-      </div>
-    </div>
-  </main>
 </body>
 
 </html>
