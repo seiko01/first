b502c89f05ba7b4e6dc5ca0b211d98aa743c246a
seiko <seiko.iwao.0715@gmail.com>
2025-01-24 07:11:24 +0900

    login/registerの修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Fortify;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ContactController;
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+
 
 Route::middleware(['auth'])->group(function() {
     Route::get('/', [ContactController::class, 'index'])->name('home');
